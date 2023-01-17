a4546c9c28ca185ff0a6f526f95a96cd4c8514eb
Dmitriy Malakhov <mitia2022@gmail.com>
2023-01-17 14:07:36 +0700

    init

--- src/routes/(auth)/auth/+page.server.ts
+++ src/routes/(auth)/auth/+page.server.ts
@@ -4,7 +4,7 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
 	const session = await event.locals.getSession();
-	if (session.user.email) {
+	if (session?.user?.email) {
 		throw redirect(300, '/');
 	}
 };
